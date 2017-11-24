444ccb92077e90abc7d396cb312e3f2295da71ab
sally-gao <sg2zv@virginia.edu>
2017-11-24 11:59:39 -0500

    put individual ingredients and amounts in a tibble

--- analysis.R
+++ analysis.R
@@ -22,38 +22,44 @@ summary(recipes_filtered)
 recipes_filtered <- recipes_filtered %>%
   mutate(ID = row_number()) 
 
-# EXTRACTING INGREDIENTS AND AMOUNTS =============================================================
+# EXTRACTING INGREDIENTS AND CORRESPONDING AMOUNTS =============================================================
 
-measures <- "(cup|teaspoon|tablespoon|can|pound|package)"
-tomatch <- paste("([[:digit:][:space:]a-z/().]+", measures, "s?)|([0-9]+[[:space:]][[:digit:]/]+)|([0-9]+)", sep="")
+measures <- "(cup|teaspoon|tablespoon|can|pound|package|clove|dash|sprig|pinch|slice)"
+tomatch <- paste("([[:digit:][:space:]a-z/().]+", measures, "s?[)]? )|([0-9]+[[:space:]][[:digit:]/]+ )|([0-9]+ )", sep="")
 
 split_amt <- function(strlist) {
-  splits <- regexpr(pattern = tomatch, strlist, useBytes=FALSE)
-  amt.stops <- attr(splits,"match.length")
-  ingredient.starts <- sapply(amt.stops, sum, 2)
-  ingredient.stops <- sapply(strlist, nchar)
-  amts <- mapply(substr, strlist, start=1, stop=amt.stops, USE.NAMES=FALSE)
-  ingredients <- mapply(substr, strlist, start=ingredient.starts, stop=ingredient.stops, USE.NAMES=FALSE)
+  
+  # split strlist into "ingredients" and "amts" vectors
+  splits <- regexpr(pattern = tomatch, strlist, useBytes=FALSE) # pattern match for amount descriptors
+  amt.stops <- sapply(attr(splits,"match.length"), sum, -1) # get amount start indices
+  ingredient.starts <- sapply(amt.stops, sum, 2) # get ingredient start indices
+  ingredient.stops <- sapply(strlist, nchar) # get ingredient stop indices
+  amt <- mapply(substr, strlist, start=1, stop=amt.stops, USE.NAMES=FALSE) # get amounts
+  ingredient <- mapply(substr, strlist, start=ingredient.starts, stop=ingredient.stops, USE.NAMES=FALSE) # get ingredients
+  
+  # deal with ingredient amounts that are specified only in ounces or fluid ounces
+  ozsplits <- regexpr(pattern = "[[:digit:][:space:]/(.]*(ounce|fluid ounce)s?[)]?", ingredients, useBytes = FALSE)
+  amts <- trimws((paste(amts, mapply(substr, ingredients, start=1, stop=attr(ozsplits, "match.length")), sep=" ")))
+  ingredient.starts <- sapply(attr(ozsplits, "match.length"), sum, 2)
+  ingredients <- mapply(substr, ingredients, start=ingredient.starts, stop=ingredient.stops, USE.NAMES=FALSE)
+  
+  # return as tibble
   df <- tibble(amts, ingredients)
-  df <- df[endsWith(df$ingredients, ":")==FALSE,]
+  df <- df[endsWith(df$ingredients, ":")==FALSE,] # remove subheadings (end with ":")
   return(df)
 }
 
-get_ingredients <- function(string) {
+get_ingredients <- function(string, id) {
   string <- gsub('\\"', "'", string) # make quotation marks uniform
   strlist <- gsub("\\[|\\]|\\'", "", unlist(strsplit(as.character(string), "', "))) # remove punctuation and split on commas
-  return(split_amt(strlist))
+  ingredients_df <- split_amt(strlist)
+  ingredients_df$recipe_id <- rep(id, nrow(ingredients_df))
+  return(ingredients_df)
 }
 
-######################################################
+all_ingredients <- tibble()
 
-get_ingredients(recipes_filtered[400, "Ingredients"])
-recipes_filtered[1700,"Ingredients"] %>% get_ingredients()
-recipes_filtered[1900,"Ingredients"] %>% get_ingredients()
-recipes_filtered[1734,"Ingredients"] %>% get_ingredients()
-recipes_filtered[1736,"Ingredients"] %>% get_ingredients()
-recipes_filtered[1738,"Ingredients"] %>% get_ingredients()
-recipes_filtered[834,"Ingredients"] %>% get_ingredients()
-recipes_filtered[835,"Ingredients"] %>% get_ingredients()
-recipes_filtered[835,"Ingredients"] %>% get_ingredients()
-# cloves? sprigs? pinch? box? pan? bag?
+for (i in 1:nrow(recipes_filtered)) {
+  all_ingredients <- all_ingredients %>% 
+    rbind(get_ingredients(recipes_filtered[i,"Ingredients"], as.numeric(recipes_filtered[i, "ID"])))
+}
