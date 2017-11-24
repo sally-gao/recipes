5421462bccff96a56baad7026204e4281341c73c
sally-gao <sg2zv@virginia.edu>
2017-11-24 17:12:34 -0500

    remove common ingredient modifiers

--- analysis.R
+++ analysis.R
@@ -22,9 +22,9 @@ summary(recipes_filtered)
 recipes_filtered <- recipes_filtered %>%
   mutate(ID = row_number()) 
 
-# EXTRACTING INGREDIENTS AND CORRESPONDING AMOUNTS =============================================================
+## EXTRACT INGREDIENTS AND CORRESPONDING AMOUNTS ====================================================================
 
-measures <- "(cup|teaspoon|tablespoon|can|pound|package|clove|dash|sprig|pinch|slice)"
+measures <- "(cup|teaspoon|tablespoon|can|pound|package|clove|dash|sprig|pinch|slice|quart|drop)"
 tomatch <- paste("([[:digit:][:space:]a-z/().]+", measures, "s?[)]? )|([0-9]+[[:space:]][[:digit:]/]+ )|([0-9]+ )", sep="")
 
 split_amt <- function(strlist) {
@@ -38,14 +38,14 @@ split_amt <- function(strlist) {
   ingredient <- mapply(substr, strlist, start=ingredient.starts, stop=ingredient.stops, USE.NAMES=FALSE) # get ingredients
   
   # deal with ingredient amounts that are specified only in ounces or fluid ounces
-  ozsplits <- regexpr(pattern = "[[:digit:][:space:]/(.]*(ounce|fluid ounce)s?[)]?", ingredients, useBytes = FALSE)
-  amts <- trimws((paste(amts, mapply(substr, ingredients, start=1, stop=attr(ozsplits, "match.length")), sep=" ")))
+  ozsplits <- regexpr(pattern = "[[:digit:][:space:]/(.]*(ounce|fluid ounce)s?[)]?", ingredient, useBytes = FALSE)
+  amt <- trimws((paste(amt, mapply(substr, ingredient, start=1, stop=attr(ozsplits, "match.length")), sep=" ")))
   ingredient.starts <- sapply(attr(ozsplits, "match.length"), sum, 2)
-  ingredients <- mapply(substr, ingredients, start=ingredient.starts, stop=ingredient.stops, USE.NAMES=FALSE)
+  ingredient <- mapply(substr, ingredient, start=ingredient.starts, stop=ingredient.stops, USE.NAMES=FALSE)
   
   # return as tibble
-  df <- tibble(amts, ingredients)
-  df <- df[endsWith(df$ingredients, ":")==FALSE,] # remove subheadings (end with ":")
+  df <- tibble(amt, ingredient)
+  df <- df[endsWith(df$ingredient, ":")==FALSE,] # remove subheadings (end with ":")
   return(df)
 }
 
@@ -53,9 +53,11 @@ get_ingredients <- function(string, id) {
   string <- gsub('\\"', "'", string) # make quotation marks uniform
   strlist <- gsub("\\[|\\]|\\'", "", unlist(strsplit(as.character(string), "', "))) # remove punctuation and split on commas
   ingredients_df <- split_amt(strlist)
-  ingredients_df$recipe_id <- rep(id, nrow(ingredients_df))
+  ingredients_df$recipe_id <- rep(id, nrow(ingredients_df)) # attach associated recipe ID to each row
   return(ingredients_df)
 }
+
+# Make final tibble
 
 all_ingredients <- tibble()
 
@@ -63,3 +65,34 @@ for (i in 1:nrow(recipes_filtered)) {
   all_ingredients <- all_ingredients %>% 
     rbind(get_ingredients(recipes_filtered[i,"Ingredients"], as.numeric(recipes_filtered[i, "ID"])))
 }
+
+## FURTHER CLEAN INGREDIENTS COLUMN ====================================================================
+
+# Check out ingredients in order of frequency of occurence
+most_common <- all_ingredients %>% 
+  group_by(ingredient) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count))
+
+# We want to remove certain words because they ingredient modifiers, not ingredients
+wordsToRemove <- c("ground", "grated", "all-purpose", "melted", "minced", "softened", "chopped", "shredded", "packed",
+                   "beaten", "dried", "sliced", "diced", "skinless", "boneless", "thighs", "thigh", "halves",
+                   "rolled", "sweetened", "lean", "distilled", "freshly", "flaked", "granulated", "unsalted", "salted",
+                   "crushed", "thawed", "extra", "virgin", "extra-virgin", "uncooked", "fresh", "large", "dry",
+                   "cold", "finely", "drained", "plain", "light", "sifted", "thinly", "rinsed", "and", "juiced", "divided",
+                   "whole", "crumbled", "separated", "firmly", "frozen", "small", "skim", "warm", "canned", "unsweetened",
+                   "confectioners", "lightly", "peeled", "refrigerated", "slightly", "breast", "breasts", "boiling",
+                   "boiled", "hot", "container", "jar", "seasoned", "unseasoned", "pitted", "trimmed", "zested", "prepared",
+                   "coarsely", "(optional)", "cooked", "cubed", "mashed", "pulled", "creamed", "whipped", "fat-free",
+                   "seeded", "julienned", "undrained", "leaves", "soaked", "low-fat", "marinated", "medium", "quartered",
+                   "heated", "cored", "powdered", "raw", "thickly", "very", "reduced-fat", "ripe", "roasted", "halved",
+                   "shelled", "smoked", "square", "squares", "squeezed", "toasted")
+
+process_ingredients <- function(ingredient_str) {
+  ingredient_str <- gsub(",", "", ingredient_str) # remove commas
+  splitstr <- unlist(strsplit(ingredient_str, " ")) # split on spaces
+  splitstr <- splitstr[(splitstr %in% wordsToRemove==FALSE)]
+  return(paste(splitstr, collapse=" "))
+}
+
+all_ingredients$ingredient <- sapply(all_ingredients$ingredient, process_ingredients, USE.NAMES = FALSE)
