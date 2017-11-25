f26bef891d1f3ec493a9b76582cf4a33a2a9b2c4
sally-gao <sg2zv@virginia.edu>
2017-11-25 15:57:43 -0500

    cleaned and processed ingredients

--- analysis.R
+++ analysis.R
@@ -24,8 +24,8 @@ recipes_filtered <- recipes_filtered %>%
 
 ## EXTRACT INGREDIENTS AND CORRESPONDING AMOUNTS ====================================================================
 
-measures <- "(cup|teaspoon|tablespoon|can|pound|package|clove|dash|sprig|pinch|slice|quart|drop)"
-tomatch <- paste("([[:digit:][:space:]a-z/().]+", measures, "s?[)]? )|([0-9]+[[:space:]][[:digit:]/]+ )|([0-9]+ )", sep="")
+measures <- "(cup|teaspoon|tablespoon|can|pound|package|clove|dash|sprig|pinch|slice|quart|drop|stalk)"
+tomatch <- paste("([[:digit:][:space:]a-z/().]+", measures, "s?[)]? )|([0-9]+[[:space:][:digit:]/]+ )|([0-9]+ )", sep="")
 
 split_amt <- function(strlist) {
   
@@ -69,12 +69,12 @@ for (i in 1:nrow(recipes_filtered)) {
 ## FURTHER CLEAN INGREDIENTS COLUMN ====================================================================
 
 # Check out ingredients in order of frequency of occurence
-most_common <- all_ingredients %>% 
+grouped_ingredients <- all_ingredients %>% 
   group_by(ingredient) %>% 
   summarise(count = n()) %>% 
   arrange(desc(count))
 
-# We want to remove certain words because they ingredient modifiers, not ingredients
+# We want to remove certain words because they are ingredient modifiers, not ingredients
 wordsToRemove <- c("ground", "grated", "all-purpose", "melted", "minced", "softened", "chopped", "shredded", "packed",
                    "beaten", "dried", "sliced", "diced", "skinless", "boneless", "thighs", "thigh", "halves",
                    "rolled", "sweetened", "lean", "distilled", "freshly", "flaked", "granulated", "unsalted", "salted",
@@ -83,16 +83,65 @@ wordsToRemove <- c("ground", "grated", "all-purpose", "melted", "minced", "softe
                    "whole", "crumbled", "separated", "firmly", "frozen", "small", "skim", "warm", "canned", "unsweetened",
                    "confectioners", "lightly", "peeled", "refrigerated", "slightly", "breast", "breasts", "boiling",
                    "boiled", "hot", "container", "jar", "seasoned", "unseasoned", "pitted", "trimmed", "zested", "prepared",
-                   "coarsely", "(optional)", "cooked", "cubed", "mashed", "pulled", "creamed", "whipped", "fat-free",
-                   "seeded", "julienned", "undrained", "leaves", "soaked", "low-fat", "marinated", "medium", "quartered",
+                   "coarsely", "cooked", "cubed", "mashed", "pulled", "creamed", "whipped", "fat-free",
+                   "seeded", "julienned", "undrained", "soaked", "low-fat", "marinated", "medium", "quartered",
                    "heated", "cored", "powdered", "raw", "thickly", "very", "reduced-fat", "ripe", "roasted", "halved",
-                   "shelled", "smoked", "square", "squares", "squeezed", "toasted")
+                   "shelled", "smoked", "square", "squares", "squeezed", "toasted", "deveined", "thick", "-", "or",
+                   "more", "semisweet", "meat", "stewed", "thin", "cut", "thick-cut", "chops", "spareribs", "tenderloin",
+                   "loin", "ribs", "roast", "sirloin", "shoulder", "mild")
+
+# pattern to match common phrases and anything in parentheses
+pattern <- "( to taste| as needed| cut into| for decoration| for dusting)|(\\(.*\\) ?)|( cut.*(strips|pieces|wedges|chunks|cubes|slices))"
 
 process_ingredients <- function(ingredient_str) {
-  ingredient_str <- gsub(",", "", ingredient_str) # remove commas
+  ingredient_str <- tolower(gsub(",", "", ingredient_str)) # remove commas and convert to lowercase
+  ingredient_str <- gsub(pattern, "", ingredient_str) # remove matches to pattern
   splitstr <- unlist(strsplit(ingredient_str, " ")) # split on spaces
-  splitstr <- splitstr[(splitstr %in% wordsToRemove==FALSE)]
+  splitstr <- splitstr[(splitstr %in% wordsToRemove==FALSE)] # remove words named above
   return(paste(splitstr, collapse=" "))
 }
 
 all_ingredients$ingredient <- sapply(all_ingredients$ingredient, process_ingredients, USE.NAMES = FALSE)
+
+# get rows whose ingredients are "salt pepper" or "salt black pepper"
+saltpepper.toreplace <- all_ingredients$ingredient=="salt pepper"|all_ingredients$ingredient=="salt black pepper"
+saltpepper.ids <- all_ingredients$recipe_id[saltpepper.toreplace] # get ids
+saltpepper.amts <- all_ingredients$amt[saltpepper.toreplace] # get amts
+
+all_ingredients <- all_ingredients[!saltpepper.toreplace,] # drop those rows
+
+# create separate rows for "salt" and "black pepper" for each id, rbind to all_ingredients
+salt <- tibble(amt = saltpepper.amts, ingredient = rep("salt", length(saltpepper.ids)), recipe_id = saltpepper.ids)
+pepper <- tibble(amt = saltpepper.amts, ingredient = rep("black pepper", length(saltpepper.ids)), recipe_id = saltpepper.ids)
+
+all_ingredients <- rbind(all_ingredients, salt, pepper)
+
+# replace every instance of "white sugar" with "sugar"
+all_ingredients[all_ingredients$ingredient=="white sugar","ingredient"] <- "sugar"
+
+# Update grouped ingredients
+grouped_ingredients <- all_ingredients %>% 
+  group_by(ingredient) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count))
+
+# Dealing with plurals: if an ingredient name +"s" or +"es" matches another ingredient name, make it the plural version
+s.plurals <- grouped_ingredients$ingredient[endsWith(grouped_ingredients$ingredient, "s")]
+s.matches <- paste(all_ingredients$ingredient, "s", sep="") %in% s.plurals
+all_ingredients$ingredient[s.matches] <- paste(all_ingredients$ingredient[s.matches], "s", sep="")
+
+es.plurals <- grouped_ingredients$ingredient[endsWith(grouped_ingredients$ingredient, "es")]
+es.matches <- paste(all_ingredients$ingredient, "es", sep="") %in% es.plurals
+all_ingredients$ingredient[es.matches] <- paste(all_ingredients$ingredient[es.matches], "es", sep="")
+
+# Remove blank ingredients
+all_ingredients <- all_ingredients[!all_ingredients$ingredient=="",]
+
+# Update grouped ingredients one final time
+grouped_ingredients <- all_ingredients %>% 
+  group_by(ingredient) %>% 
+  summarise(count = n()) %>% 
+  arrange(desc(count))
+
+## CONVERT AMTS TO UNIFORM UNITS ====================================================================
+
